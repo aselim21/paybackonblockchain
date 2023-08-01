aa4c1da4f7b0db9faf4e69a065168b417e570b10
aselim <achelia.selim@capgemini.com>
2023-08-01 11:56:34 +0200

    working

---
+++ test/pages/index.tsx
@@ -0,0 +1,11 @@
+import Copyright from "./components/Copyright";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to my Project!</h1>
+      <h2>Payback on Blockchain</h2>
+      <Copyright/>
+    </>
+  )
+}
---
+++ test/pages/payback/index.tsx
@@ -0,0 +1,240 @@
+import * as React from 'react';
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    IconButton,
+    Typography,
+    Menu,
+    Container,
+    Avatar,
+    Button, 
+    Tooltip,
+    MenuItem,
+    CssBaseline,
+    Grid,
+    TextField,
+    FormControlLabel,
+    Checkbox,
+    Link
+
+} from '@mui/material';
+import Copyright from '../components/Copyright';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const pages = ['Dashboard', 'Partner verwalten', 'Anfragen'];
+const settings = ['Einstellungen', 'Logout'];
+
+export default function Payback() {
+    return (
+        <>
+            <Navbar />
+            <ThemeProvider theme={defaultTheme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <AddPartnerForm/>
+                
+
+                    <Copyright sx={{ mt: 5 }} />
+                </Container>
+            </ThemeProvider>
+
+        </>
+    )
+}
+
+const defaultTheme = createTheme();
+const ThemePayback = createTheme({
+    palette: {
+      primary:{
+        main: '#003eb0',
+        light: '#42a5f5',
+        dark: '#1565c0',
+        contrastText: '#fff',
+      },
+      secondary:{
+        main: '#003eb0',
+        light: '#42a5f5',
+        dark: '#1565c0',
+        contrastText: '#fff',
+      }
+    },
+  });
+
+function Navbar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box
+                        component="img"
+                        sx={{
+                            // height: 233,
+                            width: "10%",
+                            // maxHeight: { xs: 233, md: 167 },
+                            // maxWidth: { xs: 350, md: 250 },
+                        }}
+                        onClick={()=>{}}
+                        alt="Payback logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Payback_Logo_2023.svg/1920px-Payback_Logo_2023.svg.png"
+                    />
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+function AddPartnerForm() {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          email: data.get('email'),
+          password: data.get('password'),
+        });
+      };
+    return (
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                {/* <LockOutlinedIcon /> */}
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign up
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            autoComplete="given-name"
+                            name="firstName"
+                            required
+                            fullWidth
+                            id="firstName"
+                            label="First Name"
+                            autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            fullWidth
+                            id="lastName"
+                            label="Last Name"
+                            name="lastName"
+                            autoComplete="family-name"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="new-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControlLabel
+                            control={<Checkbox value="allowExtraEmails" color="primary" />}
+                            label="I want to receive inspiration, marketing promotions and updates via email."
+                        />
+                    </Grid>
+                </Grid>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Sign Up
+                </Button>
+                <Grid container justifyContent="flex-end">
+                    <Grid item>
+                        <Link href="#" variant="body2">
+                            Already have an account? Sign in
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+    )
+}
