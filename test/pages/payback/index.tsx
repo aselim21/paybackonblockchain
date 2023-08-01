66d64818e4b566d4335afcebb2f0e999cd9eaf16
aselim <achelia.selim@capgemini.com>
2023-08-01 12:39:15 +0200

    partner anmelden

--- test/pages/payback/index.tsx
+++ test/pages/payback/index.tsx
@@ -8,19 +8,14 @@ import {
     Menu,
     Container,
     Avatar,
-    Button, 
+    Button,
     Tooltip,
     MenuItem,
     CssBaseline,
     Grid,
     TextField,
-    FormControlLabel,
-    Checkbox,
-    Link
-
 } from '@mui/material';
 import Copyright from '../components/Copyright';
-
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const pages = ['Dashboard', 'Partner verwalten', 'Anfragen'];
@@ -30,12 +25,10 @@ export default function Payback() {
     return (
         <>
             <Navbar />
-            <ThemeProvider theme={defaultTheme}>
+            <ThemeProvider theme={ThemePayback}>
                 <Container component="main" maxWidth="xs">
                     <CssBaseline />
-                    <AddPartnerForm/>
-                
-
+                    <AddPartnerForm />
                     <Copyright sx={{ mt: 5 }} />
                 </Container>
             </ThemeProvider>
@@ -44,23 +37,25 @@ export default function Payback() {
     )
 }
 
-const defaultTheme = createTheme();
 const ThemePayback = createTheme({
     palette: {
-      primary:{
-        main: '#003eb0',
-        light: '#42a5f5',
-        dark: '#1565c0',
-        contrastText: '#fff',
-      },
-      secondary:{
-        main: '#003eb0',
-        light: '#42a5f5',
-        dark: '#1565c0',
-        contrastText: '#fff',
-      }
-    },
-  });
+        primary: {
+            main: '#003eb0',
+            light: '#4075c0',
+            dark: '',
+            contrastText: '#fff',
+        },
+        secondary: {
+            main: '#f2f6fb',
+            light: '#fff',
+            dark: '#e5ecf6',
+            contrastText: '#000',
+        },
+        background: {
+            default: "#f2f6fb",
+        }
+    }
+});
 
 function Navbar() {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -80,20 +75,17 @@ function Navbar() {
     const handleCloseUserMenu = () => {
         setAnchorElUser(null);
     };
-
+    
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
-                <Toolbar disableGutters>
+                <Toolbar disableGutters >
                     <Box
                         component="img"
                         sx={{
-                            // height: 233,
                             width: "10%",
-                            // maxHeight: { xs: 233, md: 167 },
-                            // maxWidth: { xs: 350, md: 250 },
                         }}
-                        onClick={()=>{}}
+                        onClick={() => { }}
                         alt="Payback logo"
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Payback_Logo_2023.svg/1920px-Payback_Logo_2023.svg.png"
                     />
@@ -112,7 +104,7 @@ function Navbar() {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                <Avatar alt="Payback admin" src="/static/images/avatar/2.jpg" > P</Avatar>
                             </IconButton>
                         </Tooltip>
                         <Menu
@@ -149,11 +141,12 @@ function AddPartnerForm() {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         console.log({
-          email: data.get('email'),
-          password: data.get('password'),
+            email: data.get('email'),
+            password: data.get('password'),
         });
-      };
+    };
     return (
+        // source https://github.com/mui/material-ui/blob/v5.14.2/docs/data/material/getting-started/templates/sign-up/SignUp.tsx
         <Box
             sx={{
                 marginTop: 8,
@@ -162,60 +155,41 @@ function AddPartnerForm() {
                 alignItems: 'center',
             }}
         >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                {/* <LockOutlinedIcon /> */}
-            </Avatar>
             <Typography component="h1" variant="h5">
-                Sign up
+                Einen neuen Partner anmelden
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
                 <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            fullWidth
+                            id="partner_name"
+                            label="Name"
+                        />
+                    </Grid>
+                    <Grid item xs={12} >
+                        <TextField
+                            required
+                            fullWidth
+                            id="wallet_addr"
+                            label="Wallet Adresse"
+                        />
+                    </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
-                            autoComplete="given-name"
-                            name="firstName"
                             required
                             fullWidth
-                            id="firstName"
-                            label="First Name"
-                            autoFocus
+                            id="currency"
+                            label="Währung"
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
                             required
                             fullWidth
-                            id="lastName"
-                            label="Last Name"
-                            name="lastName"
-                            autoComplete="family-name"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="new-password"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControlLabel
-                            control={<Checkbox value="allowExtraEmails" color="primary" />}
-                            label="I want to receive inspiration, marketing promotions and updates via email."
+                            id="value_for_token"
+                            label="Wert für ein Token"
                         />
                     </Grid>
                 </Grid>
@@ -225,15 +199,9 @@ function AddPartnerForm() {
                     variant="contained"
                     sx={{ mt: 3, mb: 2 }}
                 >
-                    Sign Up
+                    anmelden
                 </Button>
-                <Grid container justifyContent="flex-end">
-                    <Grid item>
-                        <Link href="#" variant="body2">
-                            Already have an account? Sign in
-                        </Link>
-                    </Grid>
-                </Grid>
+            
             </Box>
         </Box>
     )
