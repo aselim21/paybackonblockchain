5af0fc7a930e3478e5c164cc9a61cd2366bd2542
aselim <achelia.selim@capgemini.com>
2023-08-20 13:25:38 +0200

    fix missing mandatory data message

--- test/pages/index.tsx
+++ test/pages/index.tsx
@@ -1,3 +1,25 @@
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
+  CssBaseline,
+  Grid,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+
+} from '@mui/material';
 import Copyright from "./components/Copyright";
 
 export default function Home() {
@@ -5,6 +27,27 @@ export default function Home() {
     <>
       <h1>Welcome to my Project!</h1>
       <h2>Payback on Blockchain</h2>
+
+      <Box sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                <Typography variant="h5">Accounts for testing purposes:</Typography>
+                <Typography variant="body1">
+                    (0) 0x358aae4923ff466f70ed16eedc348ac0306d8bf4 <br />
+                    (1) 0xeed4e440b4b4e170737b7c803173afcd9a08a1c3 <br />
+                    (2) 0x4a2a9a297c8a3b9fc58c22ea93bf3ff95db956fe <br />
+                    (3) 0x24660fe3d9dece8fff3b64ce548b4565157936a2 <br />
+                    (4) 0xf8a00bb5aaaa4e781ba99e3b3832d1a66eb002ea <br />
+                    (5) 0x5be309e4e9d26231931d43d8d3ddee140339d933 <br />
+                    (6) 0xc474d3441a4c203c316e729123dc4f15a05098a4 <br />
+                    (7) 0x76702dbe4c79868e3bec377a03ed3e923aacc352 <br />
+                    (8) 0x60c13ce2090581ab3411a9d5c8d933f388eba537 <br />
+                    (9) 0x93a6cbd8331b53e5276a86237bace339fc91e439 <br />
+                </Typography>
+            </Box>
       <Copyright/>
     </>
   )
--- test/pages/payback-admin/managePartners.tsx
+++ test/pages/payback-admin/managePartners.tsx
@@ -24,7 +24,6 @@ import {
 import { LoadingButton } from '@mui/lab';
 import Navbar from './components/navbar';
 import PBT_Admin from './scripts/PBT_admin';
-const home = "/payback-admin";
 
 export default function AddPartnerForm() {
     const [message, setMessage] = React.useState<string[]>(["", ""]);
@@ -41,6 +40,12 @@ export default function AddPartnerForm() {
             curr: data.get("currency")?.toString(),
             valueForToken: Number(data.get("value_for_token")),
         }
+        if (!!!req_data.name || !!!req_data.addr || !!!req_data.curr || !!!req_data.valueForToken) {
+            setMessage(["Alle Felder sind verpflichend!", `Bitte überprüfe die Daten und versuche noch einmal.`])
+            setResultIs(false);
+            setLoading(false);
+            return;
+        }
         const admin = new PBT_Admin();
         try {
             const res = await admin.addPartner(req_data.name!, req_data.addr!, req_data.curr!, req_data.valueForToken!);
@@ -50,11 +55,13 @@ export default function AddPartnerForm() {
                 setMessage(["Partner wurde erfolgreich angemeldet", `Der neue Partner hat die ID ${id}.`])
                 setResultIs(true);
                 setLoading(false);
+                return;
 
             } else {
                 setMessage(["Der Partner konnte nicht angemeldet werden.", "Bitte überprüfe die Daten und versuche noch einmal."]);
                 setResultIs(false);
                 setLoading(false);
+                return;
 
             }
 
@@ -63,7 +70,7 @@ export default function AddPartnerForm() {
             setMessage(["Der Partner konnte nicht angemeldet werden.", err.toString()]);
             setResultIs(false);
             setLoading(false);
-
+            return;
         }
     };
     return (
@@ -135,26 +142,7 @@ export default function AddPartnerForm() {
                     </LoadingButton>
                 </Box>
             </Box>
-            <Box sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}>
-                <Typography variant="h5">Accounts for testing purposes:</Typography>
-                <Typography variant="body1">
-                    (0) 0x358aae4923ff466f70ed16eedc348ac0306d8bf4 <br />
-                    (1) 0xeed4e440b4b4e170737b7c803173afcd9a08a1c3 <br />
-                    (2) 0x4a2a9a297c8a3b9fc58c22ea93bf3ff95db956fe <br />
-                    (3) 0x24660fe3d9dece8fff3b64ce548b4565157936a2 <br />
-                    (4) 0xf8a00bb5aaaa4e781ba99e3b3832d1a66eb002ea <br />
-                    (5) 0x5be309e4e9d26231931d43d8d3ddee140339d933 <br />
-                    (6) 0xc474d3441a4c203c316e729123dc4f15a05098a4 <br />
-                    (7) 0x76702dbe4c79868e3bec377a03ed3e923aacc352 <br />
-                    (8) 0x60c13ce2090581ab3411a9d5c8d933f388eba537 <br />
-                    (9) 0x93a6cbd8331b53e5276a86237bace339fc91e439 <br />
-                </Typography>
-            </Box>
+
 
 
             <Dialog
