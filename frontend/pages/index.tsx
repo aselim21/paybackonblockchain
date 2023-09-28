2e5ae140747b4da5ba9738863dfdb48e316f6708
aselim <achelia.selim@capgemini.com>
2023-09-28 17:50:44 +0200

    update first page... todo links

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -25,30 +25,43 @@ import Copyright from "./components/Copyright";
 export default function Home() {
   return (
     <>
-      <h1>Welcome to my Project!</h1>
-      <h2>Payback on Blockchain</h2>
+      <CssBaseline />
+      <AppBar position="static" sx={{ bgcolor: "#003eb0" }}>
+        <Typography variant="h2" sx={{ color: "white", alignSelf: "center", my: 2 }}>Payback on Blockchain</Typography>
+      </AppBar>
 
+      <Grid container spacing={2} sx={{ my: 10, textAlign:"center" }}>
+        <Grid item xs={12} sm={12} >
+          <Button variant="contained" sx={{ bgcolor: "#002C7D", py: 2, width: "50%"}} >Payback Admin</Button>
+        </Grid>
+        <Grid item xs={12} sm={12} sx={{ mt: 2 }}>
+          <Button variant="contained" sx={{ bgcolor: "#003eb0", py: 2, width: "50%" }} >Payback (public)</Button>
+        </Grid>
+        <Grid item xs={12} sm={12} sx={{ mt: 2 }}>
+          <Button variant="contained" sx={{ bgcolor: "#ee161f", py: 2, width: "50%" }} >Kaufland (Partner)</Button>
+        </Grid>
+      </Grid>
       <Box sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}>
-                <Typography variant="h5">Accounts for testing purposes:</Typography>
-                <Typography variant="body1">
-                    (0) 0x358aae4923ff466f70ed16eedc348ac0306d8bf4 <br />
-                    (1) 0xeed4e440b4b4e170737b7c803173afcd9a08a1c3 <br />
-                    (2) 0x4a2a9a297c8a3b9fc58c22ea93bf3ff95db956fe <br />
-                    (3) 0x24660fe3d9dece8fff3b64ce548b4565157936a2 <br />
-                    (4) 0xf8a00bb5aaaa4e781ba99e3b3832d1a66eb002ea <br />
-                    (5) 0x5be309e4e9d26231931d43d8d3ddee140339d933 <br />
-                    (6) 0xc474d3441a4c203c316e729123dc4f15a05098a4 <br />
-                    (7) 0x76702dbe4c79868e3bec377a03ed3e923aacc352 <br />
-                    (8) 0x60c13ce2090581ab3411a9d5c8d933f388eba537 <br />
-                    (9) 0x93a6cbd8331b53e5276a86237bace339fc91e439 <br />
-                </Typography>
-            </Box>
-      <Copyright/>
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+        <Typography variant="h5">Accounts for testing purposes:</Typography>
+        <Typography variant="body1">
+          (0) 0x358aae4923ff466f70ed16eedc348ac0306d8bf4 <br />
+          (1) 0xeed4e440b4b4e170737b7c803173afcd9a08a1c3 <br />
+          (2) 0x4a2a9a297c8a3b9fc58c22ea93bf3ff95db956fe <br />
+          (3) 0x24660fe3d9dece8fff3b64ce548b4565157936a2 <br />
+          (4) 0xf8a00bb5aaaa4e781ba99e3b3832d1a66eb002ea <br />
+          (5) 0x5be309e4e9d26231931d43d8d3ddee140339d933 <br />
+          (6) 0xc474d3441a4c203c316e729123dc4f15a05098a4 <br />
+          (7) 0x76702dbe4c79868e3bec377a03ed3e923aacc352 <br />
+          (8) 0x60c13ce2090581ab3411a9d5c8d933f388eba537 <br />
+          (9) 0x93a6cbd8331b53e5276a86237bace339fc91e439 <br />
+        </Typography>
+      </Box>
+      <Copyright />
     </>
   )
 }
